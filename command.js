20e5db61e1f48d62dca6235de370b460c137e3a6
Rivozn Coder <smtechofcmodz@outlook.com>
2026-02-05 09:21:45 +0300

    Update command.js

--- command.js
+++ command.js
@@ -1607,7 +1607,7 @@ reply(`${a}`)
 }
 break
 case "ask"
-case chat"
+case "chat"
 case "ai": {
     try {
         if (!text) return Reply("Please provide a message for the Ai.\nExample: `.andy what is going on`");
